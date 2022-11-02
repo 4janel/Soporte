33b2f6c39a5b9b82867455ed3860989deff834ca
Manuel Alexander <manuelalexander2004@gmail.com>
2022-11-02 15:44:11 -0600

    Inicialización del proyecto

---
+++ js/sweetAlert.js
@@ -0,0 +1,15 @@
+ Swal.fire({
+        title:'Bienvenido!',               
+        icon: 'success',
+        confirmButtonText: 'Continuar',
+        footer: 'Diseñado por la Unidad de Informática - AMSCLAE',
+        timer:5000,
+        timerProgressBar:true,
+        position: 'center',
+        allowOutsideClick:false,
+        allowEscapeKey:false,
+        allowEnterKey: false,
+        stopKeydownPropagation:false,
+   
+   });
+    
