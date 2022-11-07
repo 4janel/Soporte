818d1dbd09c5cfe8959c4be7abd9d3ca7a8bde9c
MacSoj <116688101+MacSoj@users.noreply.github.com>
2022-11-07 17:44:44 -0600

    Add files via upload

---
+++ conexion/conexion.php
@@ -0,0 +1,10 @@
+<?php 
+
+$db_host="us-cdbr-east-06.cleardb.net";
+$db_user="be9b10b45804fa";
+$db_pass="cee8bfd7";
+$db_name="heroku_d69d189ad4903c0";
+
+$conectar = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+?>
