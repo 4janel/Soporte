455a7d7ad7bd85224d123daefee02e9dd61b32e5
MacSoj <116688101+MacSoj@users.noreply.github.com>
2022-11-10 12:08:55 -0600

    Update dashbord.php

--- dashbord.php
+++ dashbord.php
@@ -1,9 +1,24 @@
 <?php   
+//Inicio de sesion 
+$info=(isset($_POST['info']))?$_POST['info']:"";
+$estado=(isset($_REQUEST['estado']))?$_REQUEST['estado']:"";
+$categoria=(isset($_REQUEST['messi']))?$_REQUEST['messi']:"";
+$prioridad=(isset($_REQUEST['prioridad']))?$_REQUEST['prioridad']:"";
+$respuesta=(isset($_POST['respuesta']))?$_POST['respuesta']:"";
+$ticket=(isset($_POST['ticket']))?$_POST['ticket']:"";
+$carnet=(isset($_POST['carnet']))?$_POST['carnet']:"";
+
+
+
 include ("conexion/conexion.php");
 $sentencia= ("SELECT Id,nombre,apellido,correo,titulo,descripcion,creado,Idticket FROM usuario,ticket WHERE usuario.Id=ticket.id_usuario");
 $listaempleados = mysqli_query($conectar, $sentencia);
 
-
+if (isset($_POST["enviar"])){
+  $sentencia= ("INSERT INTO informacion(Idinfo, nestado, categoria, prioridad, respuesta, ID_ticket, usuario_id) VALUES ('$info','$estado','$categoria','$prioridad', '$respuesta', '$ticket', '$carnet')");
+  $query = mysqli_query($conectar, $sentencia);
+  echo '<div class="alert alert-danger">REGISTRO INGRESADO</div>';
+}
 ?>
 
 <!DOCTYPE html>
@@ -28,7 +43,6 @@ $listaempleados = mysqli_query($conectar, $sentencia);
 </head>
 
 <body>
-
   <!-- ======= Encabezado ======= -->
   <header id="header" class="header fixed-top d-flex align-items-center">
 
@@ -45,7 +59,7 @@ $listaempleados = mysqli_query($conectar, $sentencia);
       <h1>Panel de administración</h1>
       <nav>
         <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="index.php">Cerrrar sesión</a></li>
+          <li class="breadcrumb-item"><a href="index.html">Cerrrar sesión</a></li>
         </ol>
       </nav>
     </div>
@@ -74,6 +88,7 @@ $listaempleados = mysqli_query($conectar, $sentencia);
                         <th scope="col">Descripción</th>
                         <th scope="col">Fecha de envio</th>
                         <th scope="col">Id Ticket</th>
+                        <th scope="col">Consulta</th>
                       </tr>
                     </thead>
                     <tbody>
@@ -86,11 +101,16 @@ $listaempleados = mysqli_query($conectar, $sentencia);
                         <td><?php echo $registro['descripcion']; ?></td> 
                         <td><?php echo $registro['creado']; ?></td>
                         <td><?php echo $registro['Idticket']; ?></td>
+
+                        <td>
+                        <form action="" method="POST">
+                        <input value="Seleccionar" type="submit" class="btn btn-info"  name="accion">             
+                        </td>
+
                     </tr>
                     <?php }; ?>
                     </tbody>
                   </table>
-
                 </div>
 
               </div>
@@ -101,6 +121,78 @@ $listaempleados = mysqli_query($conectar, $sentencia);
       </div>
     </section>
 
+      <!-- FORMS -->
+    <section class="section">
+      <div class="row">
+        <div class="col-lg-6">
+
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Respuesta tickets</h5>
+
+              <!-- General Form Elements -->
+              <form action="" method="POST" enctype="multipart/form-data">
+                <div class="row mb-3">
+                  <label for="inputText">No información</label>
+                  <div class="col-sm-10">
+                    <input type="text" name="info" id="info" class="form-control">
+                  </div>
+                </div>
+
+                <label for="inputText">Estado</label>
+                <select class="form-select" aria-label="Default select example" name="estado">
+                    <option selected>Estado</option>
+                    <option value="Abierto">Abierto</option>
+                    <option value="Cerrado">Cerrado</option>
+                </select>
+
+                <label for="inputText">Categoria</label>
+                <select class="form-select" aria-label="Default select example" name="messi">
+                    <option selected>Categoria</option>
+                    <option value="Soporte técnico">Soporte técnico</option>
+                    <option value="Ayuda">Ayuda</option>
+                </select>
+
+                <label for="inputText">Prioridad</label>
+                <select class="form-select" aria-label="Default select example" name="prioridad">
+                    <option selected>Prioridad</option>
+                    <option value="Alta">Alta</option>
+                    <option value="Baja">Baja</option>
+                </select>
+
+                <div class="row mb-3">
+                  <label for="inputNumber">Respuesta</label>
+                  <div class="col-sm-10">
+                    <textarea name="respuesta" id="respuesta" cols="30" rows="10" class="form-control"></textarea>
+                  </div>
+                </div>
+                <div class="row mb-3">
+                  <label for="inputNumber">Id ticket</label>
+                  <div class="col-sm-10">
+                    <input name="ticket" id="ticket" class="form-control" type="text" id="formFile">
+                  </div>
+                </div>
+                <div class="row mb-3">
+                  <label for="inputNumber">Carnet</label>
+                  <div class="col-sm-10">
+                    <input name="carnet" id="carnet" class="form-control" type="text" id="formFile">
+                  </div>
+                </div>
+
+                <div class="row mb-3">
+                  <div class="col-sm-10">
+                    <button type="submit" class="btn btn-primary" name="enviar">Enviar respuesta</button>
+                  </div>
+                </div>
+
+              </form><!-- End General Form Elements -->
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
   </main>
 
   <!-- JS archivo -->
