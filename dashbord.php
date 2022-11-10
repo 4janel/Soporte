9a7266585e42bc8a8d166818b3d9dbcfb3385d0c
MacSoj <116688101+MacSoj@users.noreply.github.com>
2022-11-09 23:58:03 -0600

    Update dashbord.php

--- dashbord.php
+++ dashbord.php
@@ -10,8 +10,8 @@ $listaempleados = mysqli_query($conectar, $sentencia);
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
+    <meta charset="utf-8">
 
   <title>Tickets UVG</title>
   
