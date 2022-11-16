b4d1b28bd4f55ac2c8d767e6d7cbdaea6277b6d7
MacSoj <116688101+MacSoj@users.noreply.github.com>
2022-11-16 00:04:38 -0600

    Update respuesta.php

--- administrador/dashboard/respuesta.php
+++ administrador/dashboard/respuesta.php
@@ -6,10 +6,13 @@ $prioridad=(isset($_REQUEST['prioridad']))?$_REQUEST['prioridad']:"";
 $respuesta=(isset($_POST['respuesta']))?$_POST['respuesta']:"";
 $ticket=(isset($_POST['ticket']))?$_POST['ticket']:"";
 $carnet=(isset($_POST['carnet']))?$_POST['carnet']:"";
+$idticket=(isset($_POST['idticket']))?$_POST['idticket']:"";
+$idinfo=(isset($_POST['idinfo']))?$_POST['idinfo']:"";
 
 
 
 include ("../../conexion/conexion.php");
+
 
 
 if (isset($_POST["enviar"])){
@@ -24,5 +27,15 @@ if (isset($_POST["enviar"])){
     echo '<script>alert("Se ha enviado la respuesta.");</script>';
     echo '<script type="text/javascript"> window.location="./";</script>';
   }
+}
+
+
+if (isset($_POST["Eliminar"])){
+
+    $sentencia2= ("DELETE informacion,ticket from informacion,ticket where informacion.Idinfo=$idinfo and ticket.Idticket=$idticket");
+    $query = mysqli_query($conectar, $sentencia2);
+    echo '<script>alert("Se ha eliminado el ticket.");</script>';
+    echo '<script type="text/javascript"> window.location="./";</script>';
+
 }
 ?>
